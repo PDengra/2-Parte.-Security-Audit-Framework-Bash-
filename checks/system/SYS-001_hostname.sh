43af6e71d9533da451b79ad1f3b51369988692af
Pablo Dengra <164612744+PDengra@users.noreply.github.com>
2026-01-20 09:48:32 +0100

    Update SYS-001_hostname.sh

--- checks/system/SYS-001_hostname.sh
+++ checks/system/SYS-001_hostname.sh
@@ -1,1 +1,34 @@
+#!/usr/bin/env bash
+# ==========================================================
+# SYS-001 - Hostname properly configured
+# ==========================================================
+
+CHECK_ID="SYS-001"
+CHECK_NAME="System hostname properly configured"
+CHECK_CATEGORY="System"
+CHECK_SEVERITY="LOW"
+CHECK_PROFILES=("server" "workstation" "hardened")
+
+run_check() {
+    local hostname
+    hostname="$(hostname)"
+
+    if [[ -z "$hostname" ]]; then
+        check_fail \
+            "Hostname is empty" \
+            "Configure a valid hostname using hostnamectl"
+        return
+    fi
+
+    case "$hostname" in
+        localhost|localhost.localdomain)
+            check_fail \
+                "Hostname is set to ${hostname}" \
+                "Set a unique hostname using hostnamectl"
+            ;;
+        *)
+            check_pass "Hostname configured: ${hostname}"
+            ;;
+    esac
+}
 
