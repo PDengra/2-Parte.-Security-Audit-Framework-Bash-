90780ad8b044965673359e0c6baf271d6e5ade9f
Pablo Dengra <164612744+PDengra@users.noreply.github.com>
2026-01-20 09:48:50 +0100

    Update SYS-002_time_sync.sh

--- checks/system/SYS-002_time_sync.sh
+++ checks/system/SYS-002_time_sync.sh
@@ -1,1 +1,27 @@
+#!/usr/bin/env bash
+# ==========================================================
+# SYS-002 - Time synchronization enabled
+# ==========================================================
+
+CHECK_ID="SYS-002"
+CHECK_NAME="System time synchronized"
+CHECK_CATEGORY="System"
+CHECK_SEVERITY="MEDIUM"
+CHECK_PROFILES=("server" "workstation" "hardened")
+
+run_check() {
+    if command_exists timedatectl; then
+        if timedatectl | grep -q "System clock synchronized: yes"; then
+            check_pass "System time is synchronized"
+        else
+            check_fail \
+                "Time synchronization disabled" \
+                "Enable NTP using systemd-timesyncd or chrony"
+        fi
+    else
+        check_warn \
+            "timedatectl not available" \
+            "Verify time synchronization manually"
+    fi
+}
 
